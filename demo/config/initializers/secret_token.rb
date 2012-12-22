5001570dbeb543ff851b5ab5511c3e9ee2b07765
Michael DeWitt <md@hackfoolery.com>
2012-12-21 22:01:24 -0500

    scaffolding for first app

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'a616956f0977b47c506473a1525a6d73f3b9a9c4abe409fc2c3a78b44a477df245b29b13b315d18f55e0665524dde8f0c840b76f3c66fba698d48a5fd0c2f328'
